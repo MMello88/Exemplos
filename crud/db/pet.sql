f7298c828bad856640afa973c50efd5d030c8867
MattMello88 <matheusnarciso@hotmail.com>
2022-05-30 17:41:38 -0300

    db

--- crud/db/pet.sql
+++ crud/db/pet.sql
@@ -23,10 +23,36 @@ DROP TABLE IF EXISTS `atividades`;
 CREATE TABLE `atividades` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `nome` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `ativo` enum('Sim','Não') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Sim',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `atividades` */
+
+insert  into `atividades`(`id`,`nome`,`ativo`) values (1,'Matheus','Sim'),(2,'Teste','Sim'),(3,'ABC','Sim');
+
+/*Table structure for table `cartoes` */
+
+DROP TABLE IF EXISTS `cartoes`;
+
+CREATE TABLE `cartoes` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `tipo` enum('Cartão de Crédito','Cartão de Débito') NOT NULL,
+  `numero` decimal(24,0) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `dt_expiracao` date NOT NULL,
+  `cvv` decimal(3,0) DEFAULT NULL,
+  `bandeira` enum('Visa','Master Card','Elo','American Express') DEFAULT NULL,
+  `usuario_id` bigint(20) NOT NULL,
+  `ativo` enum('Sim','Não') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_usuario_cartao` (`usuario_id`),
+  CONSTRAINT `fk_usuario_cartao` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+/*Data for the table `cartoes` */
+
+insert  into `cartoes`(`id`,`tipo`,`numero`,`nome`,`dt_expiracao`,`cvv`,`bandeira`,`usuario_id`,`ativo`) values (2,'Cartão de Débito',1234123412341234,'Clinica Matheus','2022-08-11',123,'Visa',1,'Não'),(3,'Cartão de Crédito',1234567891234567,'matheus','2022-06-02',123,'Master Card',1,'Sim'),(6,'Cartão de Crédito',1243414341241,'teste','2022-06-02',123,'Elo',1,'Sim'),(7,'Cartão de Débito',9875465488798754,'cartão teste','2022-06-01',123,'Visa',1,'Sim');
 
 /*Table structure for table `empresas` */
 
@@ -47,12 +73,15 @@ CREATE TABLE `empresas` (
   `celular` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
   `pago` enum('Sim','Não') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Não',
   `dt_experiencia` date DEFAULT NULL,
+  `ativo` enum('Sim','Não') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Sim',
   PRIMARY KEY (`id`),
   KEY `empresas_atividade_id_foreign` (`atividade_id`),
   CONSTRAINT `empresas_atividade_id_foreign` FOREIGN KEY (`atividade_id`) REFERENCES `atividades` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `empresas` */
+
+insert  into `empresas`(`id`,`atividade_id`,`razao_social`,`nome_fantasia`,`cep`,`endereco`,`numero`,`bairro`,`complemento`,`cidade`,`uf`,`celular`,`pago`,`dt_experiencia`,`ativo`) values (20,2,'Teste','MATHEUS DE MELLO IZABEL CRISTINA BUGNOLA DE MELLO','14015-170','Rua Álvares Cabral','40339455','centro','AP 51','Ribeirão Preto','AS','991838523','Não','2022-05-27','Sim'),(21,1,'Matheus de Mello','Matheus Mello','14015170','AV 9 DE JULHO','1243124234','test','test','Ribeirão Preto, city, Brazil','AS','(16) 66666-6666','Não','2022-05-18','Sim');
 
 /*Table structure for table `enderecos` */
 
@@ -71,12 +100,15 @@ CREATE TABLE `enderecos` (
   `telefone` varchar(20) NOT NULL,
   `principal` enum('Sim','Não') DEFAULT NULL,
   `usuario_id` bigint(20) NOT NULL,
+  `ativo` enum('Sim','Não') NOT NULL DEFAULT 'Sim',
   PRIMARY KEY (`id`),
   KEY `fk_usuario_endereco` (`usuario_id`),
   CONSTRAINT `fk_usuario_endereco` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 /*Data for the table `enderecos` */
+
+insert  into `enderecos`(`id`,`nome`,`rua`,`numero`,`bairro`,`complemento`,`cep`,`estado`,`cidade`,`telefone`,`principal`,`usuario_id`,`ativo`) values (1,'teste','teste','teste','teste','teste','teste','te','test','teste','Não',1,'Não'),(3,'Matheus','Miguel','asf','adf','adf','af','ad','asf','asf','Não',1,'Sim'),(4,'matheus','xxxxx','xxx','xx','xxx','xxx','xx','xxxx','xxx','Não',1,'Sim'),(5,'sergio','yyy','y','y','y','y','y','y','y','Não',1,'Não'),(6,'Clinica Matheus','Miguel','40339455','centro','AP 51','14180000','ad','Ribeirão Preto','+5516991838523','Sim',1,'Sim');
 
 /*Table structure for table `pages` */
 
@@ -88,13 +120,14 @@ CREATE TABLE `pages` (
   `param` varchar(255) NOT NULL,
   `value` varchar(4000) DEFAULT NULL,
   `valueImg` varchar(255) DEFAULT NULL,
+  `ativo` enum('Sim','Não') NOT NULL DEFAULT 'Sim',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_param` (`tipo`,`param`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 /*Data for the table `pages` */
 
-insert  into `pages`(`id`,`tipo`,`param`,`value`,`valueImg`) values (1,'header','titulo','Bem vindo ao Pets',NULL),(2,'header','meta','pet; laboratório; petshop; ',NULL);
+insert  into `pages`(`id`,`tipo`,`param`,`value`,`valueImg`,`ativo`) values (1,'header','titulo','Bem vindo ao Pets',NULL,'Sim'),(2,'header','meta','pet; laboratório; petshop; ',NULL,'Sim');
 
 /*Table structure for table `usuario` */
 
@@ -115,11 +148,11 @@ CREATE TABLE `usuario` (
   UNIQUE KEY `uk_email` (`email`),
   KEY `users_empresa_id_foreign` (`empresa_id`),
   CONSTRAINT `users_empresa_id_foreign` FOREIGN KEY (`empresa_id`) REFERENCES `empresas` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 /*Data for the table `usuario` */
 
-insert  into `usuario`(`id`,`nome`,`email`,`senha`,`tipo`,`avatar`,`cpf_cnpj`,`ativo`,`telefone`,`empresa_id`) values (1,'Matheus de Mello','matheusnarciso@hotmail.com','e10adc3949ba59abbe56e057f20f883e','Laboratório',NULL,'36848874809','Sim','16991838523',NULL);
+insert  into `usuario`(`id`,`nome`,`email`,`senha`,`tipo`,`avatar`,`cpf_cnpj`,`ativo`,`telefone`,`empresa_id`) values (1,'Matheus de Mello','matheusnarciso@hotmail.com','e10adc3949ba59abbe56e057f20f883e','Administrador',NULL,'36848874809','Sim','16991838523',21),(2,'','','','Administrador',NULL,NULL,'Sim',NULL,NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
